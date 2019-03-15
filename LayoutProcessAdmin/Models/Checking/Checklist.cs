ffdbf913c294b098fea532159bab4600bfbdcc8f
Manthra <daniel.1912@hotmail.com>
2019-03-15 10:02:38 -0600

    Trabajando en el diseño de create checklists

--- LayoutProcessAdmin/Models/Checking/Checklist.cs
+++ LayoutProcessAdmin/Models/Checking/Checklist.cs
@@ -12,17 +12,21 @@ namespace LayoutProcessAdmin.Models.Checking
 
         [Required]
         [StringLength(30)]
+        [Display(Name = "Code")]
         public string chr_Clave { get; set; }
 
         [Required]
         [StringLength(30)]
+        [Display(Name = "Name")]
         public string chr_Name { get; set; }
 
         [StringLength(100)]
+        [Display(Name = "Description")]
         public string chr_Description { get; set; }
         
         public Period int_Period { get; set; }
 
+        [Display(Name = "Active")]
         public bool bit_Activo { get; set; }
 
         public User int_Owner { get; set; }
