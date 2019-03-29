808182559ff75daecf6a65a1359182e7f6c2eab6
Manthra <daniel.1912@hotmail.com>
2019-03-29 14:04:09 -0600

    Añadido el acceso al controlador de areas desde layout

--- LayoutProcessAdmin/Controllers/AreasController.cs
+++ LayoutProcessAdmin/Controllers/AreasController.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using LayoutProcessAdmin.Models;
+using LayoutProcessAdmin.Models.Areas;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
-using LayoutProcessAdmin.Models;
-using LayoutProcessAdmin.Models.Areas;
 
 namespace LayoutProcessAdmin.Controllers
 {
