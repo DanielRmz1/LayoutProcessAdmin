74b13d90ba9c9a370a0ed1501c15d898758fa0dc
Manthra <daniel.1912@hotmail.com>
2019-04-08 16:32:27 -0500

    Añadidos los modelos de auditoria

---
+++ LayoutProcessAdmin/Models/Auditing/Audit.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LayoutProcessAdmin.Models.Auditing
+{
+    [Table("Tbl_Audit")]
+    public class Audit
+    {
+        [Key]
+        public int int_IdAudit { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        [Display(Name = "Name")]
+        public string chr_Name { get; set; }
+
+        [StringLength(200)]
+        [Display(Name = "Description")]
+        public string chr_Description { get; set; }
+
+        /// <summary>
+        /// Description: 
+        /// Audit -> 0
+        /// Encuesta -> 1
+        /// Mas tipos proximamente xD
+        /// </summary>
+        [Display(Name = "Type")]
+        public int int_Type { get; set; }
+    }
+}
---
+++ LayoutProcessAdmin/Models/Auditing/AuditConfig.cs
@@ -0,0 +1,19 @@
+﻿using LayoutProcessAdmin.Models.Checking;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LayoutProcessAdmin.Models.Auditing
+{
+    [Table("Tbl_AuditConfig")]
+    public class AuditConfig
+    {
+        [Key]
+        public int int_IdAuditConfig { get; set; }
+
+        public int int_Level { get; set; }
+
+        public Checklist int_Checklist { get; set; }
+        
+        public Audit Audit { get; set; }
+    }
+}
