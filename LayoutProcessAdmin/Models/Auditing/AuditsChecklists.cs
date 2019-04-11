5fb5d8dea027dbb994d1edb7a1520fffbffea74b
Manthra <daniel.1912@hotmail.com>
2019-04-11 13:08:30 -0500

    Añadido modelo para relacionar auditorias con checklists

---
+++ LayoutProcessAdmin/Models/Auditing/AuditsChecklists.cs
@@ -0,0 +1,18 @@
+﻿using LayoutProcessAdmin.Models.Checking;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LayoutProcessAdmin.Models.Auditing
+{
+    [Table("Tbl_AuditsChecklists")]
+    public class AuditsChecklists
+    {
+        [Key]
+        public int int_IdAuditsChecklists { get; set; }
+
+        public AuditConfig AuditConfig { get; set; }
+
+        public Checklist Checklist { get; set; }
+
+    }
+}
